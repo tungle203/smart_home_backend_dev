6a0b1f6a7b9ed480be421520952b99bf0c616b34
tungle203 <thanhtunga52021@gmail.com>
2024-05-03 15:47:43 +0700

    Update Redis configuration and add .env.example file

--- src/config/session.js
+++ src/config/session.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
 const session = require('express-session');
 const Redis = require('ioredis');
 const RedisStore = require('connect-redis').default;
-const clientRedis = new Redis();
-require('dotenv').config();
+const clientRedis = new Redis({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+    }
+});
 
 const sessionConfig = {
     store: new RedisStore({ client: clientRedis }),
